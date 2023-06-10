7a70433543b679dbafb3570865d008b6d58d2d7f
yasumori <ysmr@protonmail.com>
2023-06-10 01:50:25 -0400

    read a file line by line

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"os"
 	"io/fs"
 	"fmt"
 	_ "regexp"
 )
+
+// we use fs.File, not os.File because fs.File is an interface that implements Read
+func processFile(file fs.File) {
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	count := 0
+
+	for inputLeft := scanner.Scan(); inputLeft ; inputLeft = scanner.Scan(){
+		count++
+		line := scanner.Text()
+		fmt.Printf("line: %d:%s\n", count, line)
+	}
+}
 
 /*
 grep [flags] patterns [file]
@@ -22,8 +36,11 @@ func main() {
 	if f.Mode() & fs.ModeCharDevice == 0 {
 		// grab from stdin
 		fmt.Println("From stdin")
+		processFile(os.Stdin)
 	} else {
 		// grab from file
 		fmt.Println("From file")
 	}
+
+
 }
