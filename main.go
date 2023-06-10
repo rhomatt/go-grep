a30b6a9a1395f5d8de385ee31f5c19a868aa6451
yasumori <ysmr@protonmail.com>
2023-06-10 02:54:24 -0400

    func to process line

--- main.go
+++ main.go
@@ -5,11 +5,23 @@ import (
 	"os"
 	"io/fs"
 	"fmt"
-	_ "regexp"
+	"regexp"
 )
 
+// return a match if found
+func processLine(line , pattern string) (string, bool) {
+	re := regexp.MustCompilePOSIX(pattern)
+	match := re.FindString(line)
+
+	if match != "" {
+		return match, true
+	}
+
+	return "", false
+}
+
 // we use fs.File, not os.File because fs.File is an interface that implements Read
-func processFile(file fs.File) {
+func processFile(file fs.File) int {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 	count := 0
@@ -18,21 +30,27 @@ func processFile(file fs.File) {
 		count++
 		line := scanner.Text()
 		fmt.Printf("line: %d:%s\n", count, line)
+
 	}
+
+	return count
 }
 
 /*
 grep [flags] patterns [file]
 goals: 
-support multiple patterns
 support -r, -i
 support stdin and file
 */
 func main() {
+
+
+	// check if input is piped
 	f, e := os.Stdin.Stat()
 	if e != nil {
 		panic(e)
 	}
+
 	if f.Mode() & fs.ModeCharDevice == 0 {
 		// grab from stdin
 		fmt.Println("From stdin")
